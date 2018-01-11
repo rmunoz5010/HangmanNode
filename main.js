8040476c7d3de5a5646d34d96e85126383bb68c9
Ramon Munoz <rgm_munoz@mac.com>
2018-01-11 09:18:57 -0500

    v1.8

---
+++ main.js
@@ -0,0 +1,5 @@
+var inquirer = require('inquirer');
+var isLetter = require('is-letter');
+var Words = require('./words.js');
+var Game = require('./game.js');
+var hangManDisplay = Game.newWord.hangman;
